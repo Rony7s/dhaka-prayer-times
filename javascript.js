e44bb9740a3268aa74ff874de09799a7618af2fb
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2024-11-12 21:38:17 +0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,44 @@
+//for today
+function getCurrentMonthAndDay() {
+    const date = new Date();
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    
+    return [month, day];
+}
+
+//console.log(getCurrentMonthAndDay()[0]);
+function todayTable(m, i) {
+    i = i - 1;
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const selectedData = data[m];
+            const tbody = document.querySelector('#today_prayer_times tbody');
+            tbody.innerHTML = '';  // Clear previous data
+
+            const row = document.createElement('tr');
+            const prayerTimes = ['date', 'fajr', 'sunrise', 'dhuhr', 'asr', 'maghrib', 'isha'];
+
+            // Loop through each prayer time and create a table cell
+            prayerTimes.forEach(time => {
+                const cell = document.createElement('td');
+                cell.textContent = selectedData[time][i];
+                row.appendChild(cell);
+            });
+
+            tbody.appendChild(row);
+        })
+        .catch(error => {
+            console.error('Error loading data:', error);
+        });
+}
+
+
+
 document.getElementById('month-select').addEventListener('change', function() {
     const selectedMonth = this.value;
 
@@ -49,5 +90,7 @@ document.getElementById('month-select').addEventListener('change', function() {
         });
 });
 
+
+todayTable(getCurrentMonthAndDay()[0],getCurrentMonthAndDay()[1]);
 // Initial load for January data
 document.getElementById('month-select').dispatchEvent(new Event('change'));
